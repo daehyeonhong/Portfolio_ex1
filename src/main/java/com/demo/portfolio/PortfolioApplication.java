031ec26a401dcb559993dbbd7527cf8b8632e9fb
DaeHyeonHong <fholng@outlook.com>
2022-12-28 21:59:43 +0900

    [Config] ✏️스프링 기본 설정 1. 프로젝트 구성 2. DB 설정 3. log 설정(보완 필요)

--- src/main/java/com/demo/portfolio/PortfolioApplication.java
+++ src/main/java/com/demo/portfolio/PortfolioApplication.java
@@ -2,12 +2,14 @@ package com.demo.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
 
 }
--- src/test/java/com/demo/portfolio/PortfolioApplicationTests.java
+++ src/test/java/com/demo/portfolio/PortfolioApplicationTests.java
@@ -1,13 +1,15 @@
 package com.demo.portfolio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PortfolioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
 
 }
