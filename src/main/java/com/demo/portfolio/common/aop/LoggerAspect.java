bf0317ded6a64a941d117897e4c4ef62dac12e23
DaeHyeonHong <fholng@outlook.com>
2022-12-29 22:08:38 +0900

    [Config] Jacoco 및 로깅 설정 1. Jacoco 기능 기본 세팅 2. 로깅 에러 처리

---
+++ src/main/java/com/demo/portfolio/common/aop/LoggerAspect.java
@@ -0,0 +1,30 @@
+package com.demo.portfolio.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @Around("@annotation(org.springframework.web.bind.annotation.RestController)")
+    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        final Signature signature = joinPoint.getSignature();
+        log.info("{}.{}()", signature.getDeclaringTypeName(), signature.getName());
+        return joinPoint.proceed();
+    }
+
+    @Around("@annotation(org.springframework.stereotype.Repository)")
+    public Object printArgs(ProceedingJoinPoint joinPoint) throws Throwable {
+        final Signature signature = joinPoint.getSignature();
+        log.info("{}.{}()", signature.getDeclaringTypeName(), signature.getName());
+        log.info("{}", joinPoint.getArgs());
+        return joinPoint.proceed();
+    }
+
+}
